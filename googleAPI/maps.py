fa7946609906a3313f92ceb6c155e8568fd7e965
bped14 <brpe0567@colorado.edu>
2017-04-08 23:05:08 -0600

    finalized and tested drive time data retrieval, test was successful

--- googleAPI/maps.py
+++ googleAPI/maps.py
@@ -3,17 +3,28 @@
 import urllib2
 import json
 
+#dictionary of resorts for testing
+#<script>
+
 ski_resorts = {
     "Keystone":{'city': 'Keystone', 'state': 'CO'},
     "Breckenridge":{'city': 'Breckenridge', 'state': 'CO'},
-    "Winter Park":{'city':'Winter Park', 'state': 'CO'},
-    "Copper":{'city':'Copper', 'state':'CO'}
+    "Winter Park":{'city':'Winter+Park', 'state': 'CO'},
+    "Copper":{'city':'Copper', 'state':'CO'},
+    "Eldora":{'city':'Eldora', 'state':'CO'},
+    "Loveland":{'city':'Dillon', 'state':'CO'},
+    "Arapahoe Basin":{'city':'A-Basin+Ski+Resort', 'state':'CO'}
     }
 
 for resort in ski_resorts:
     request = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Boulder,CO&destinations={0},{1}&key=AIzaSyDSMcE-OZ0c96S6CWFi5FXRRf6_RoVt7Cc'.format(ski_resorts[resort]['city'],ski_resorts[resort]['state'])
-    response = urllib.urlopen(request)
+    response = urllib2.urlopen(request)
     data = json.loads(response.read())
-    distance = data[distance][value]
+    text = data["rows"][0]["elements"][0]["duration"]["text"]
+    value = data["rows"][0]["elements"][0]["duration"]["value"]
+    time = json.dumps(text)
+    number = json.dumps(value)
 
-    print distance
+    print time
+    print number
+#</script>
